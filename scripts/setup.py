f071b89423ac12dc20409b1cfa3cc0e1a3478af7
WINS Laboratory <cengwins@gmail.com>
2021-09-06 13:44:43 +0300

    path added

--- scripts/setup.py
+++ scripts/setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup, Extension
 if sys.platform == 'darwin':
     cpp_args = ['-w', '-std=c++11', '-g','-stdlib=libc++', '-mmacosx-version-min=10.7']
 else:
-    cpp_args = ['-w', '-std=c++11', '-g', '-I/usr/local/lib/python3.8/dist-packages/pybind11']
+    cpp_args = ['-w', '-std=c++11', '-g', '-I/usr/local/lib/python3.8/dist-packages/pybind11/include']
 
 #paths=sysconfig.get_paths()
 #for key, value in paths.items():

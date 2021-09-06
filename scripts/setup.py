81b01f04c5612b24bd13204b94c66f0ebcec9d61
WINS Laboratory <cengwins@gmail.com>
2021-09-06 12:07:02 +0300

    paths added

--- scripts/setup.py
+++ scripts/setup.py
@@ -7,9 +7,12 @@ cpp_args = ['-w', '-std=c++11', '-g']
 paths=sysconfig.get_paths()
 for key, value in paths.items():
     aKey = key
-    aValue = value
+    aValue = "-I" + value
     cpp_args.append(aValue)
+    
 link_args = ['-v', '-L.', '-L/usr/local/lib', '-lfec', '-lboost_system', '-lpthread', '-luhd', '-lliquid', '-lm', '-lc']
+
+
 ext_modules = [
     Extension(
     'liquid_usrp_pybind11_wrapper',
